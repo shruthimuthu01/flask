02d4011fb5c0a1ca2f257546627c83b86ed60cb4
shruthimuthu01 <63283712+shruthimuthu01@users.noreply.github.com>
2022-12-10 12:33:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
     return "hello"
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    return jsonify({'Alert':'be happ'})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
